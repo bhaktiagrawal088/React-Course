ecbad5d44a5966e79636f38072fc1e24705ae3f9
unknown <bhaktiagrawal397@gmail.com>
2024-06-29 15:17:53 +0530

    Todo

---
+++ 10TodoContext/src/contexts/Todocontexts.js
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+
+export const Todocontexts = createContext({
+    Todos : [{
+        id : 1,
+        todo : "Learn React",
+        completed : false,
+    }],
+    addTodo : (todo) => {},
+    updateTodo : (id, todo) => {},
+    deleteTodo : (id) => {},
+    toggleComplete : (id) => {},
+})
+
+
+export const Todouse = () =>{
+    return useContext(Todocontexts)
+}
+
+export const TodoProvider = Todocontexts.Provider
---
+++ 10TodoContext/src/contexts/index.js
@@ -0,0 +1,1 @@
+export { TodoProvider, Todouse , Todocontexts } from './Todocontexts'
